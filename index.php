0725f5ba2e2b65540ee99fdcd1a5fc5b4e2a8791
Aaron <charlesjimenez0705@gmail.com>
2025-10-07 20:19:16 +0800

    week 1/2 milestone update

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>EduNotes</title>
+</head>
+<body>
+  <h1>Welcome to EduNotes</h1>
+  <a href="read.php">📖 View Notes</a> |
+  <a href="create.php">➕ Add New Note</a>
+</body>
+</html>
